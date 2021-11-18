dffe9ba874218cee445343611f518f230f51a8ed
Benjamin Saunders <ben.e.saunders@gmail.com>
2021-11-17 19:23:51 -0800

    Remove unnecessary Arc

--- quinn/src/endpoint.rs
+++ quinn/src/endpoint.rs
@@ -363,7 +363,7 @@ impl Drop for EndpointDriver {
 #[derive(Debug)]
 pub(crate) struct EndpointInner {
     socket: UdpSocket,
-    udp_state: Arc<UdpState>,
+    udp_state: UdpState,
     inner: proto::Endpoint,
     outgoing: VecDeque<proto::Transmit>,
     incoming: VecDeque<Connecting>,
@@ -571,7 +571,7 @@ impl EndpointRef {
         let (dirty_send, dirty) = mpsc::unbounded_channel();
         Self(Arc::new(Mutex::new(EndpointInner {
             socket,
-            udp_state: Arc::new(UdpState::new()),
+            udp_state: UdpState::new(),
             inner,
             ipv6,
             outgoing: VecDeque::new(),
